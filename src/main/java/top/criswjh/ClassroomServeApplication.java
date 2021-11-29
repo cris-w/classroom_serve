db687961b39349b899f95405d32df565c6c83ee7
wjh <1508710375@qq.com>
2021-11-29 17:55:31 +0800

    init

---
+++ src/main/java/top/criswjh/ClassroomServeApplication.java
@@ -0,0 +1,16 @@
+package top.criswjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wjh
+ */
+@SpringBootApplication
+public class ClassroomServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassroomServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/criswjh/ClassroomServeApplicationTests.java
@@ -0,0 +1,13 @@
+package top.criswjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
